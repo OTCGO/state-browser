79325f665c0b372425b35d19a109190e2a9e3f96
503945930 <503945930@qq.com>
2018-03-13 22:33:14 +0800

    update window.GetUrlParam = function(name){

--- config.js
+++ config.js
@@ -1,24 +1,36 @@
 let host = 'http://future.otcgo.cn:5001'
 let network = 'testnet'
 
-window.GetUrlParam = function (paraName) {
-　　　　var url = document.location.toString();
-　　　　var arrObj = url.split("?");
+// window.GetUrlParam = function (paraName) {
+// 　　　　var url = document.location.toString();
+// 　　　　var arrObj = url.split("?");
 
-　　　　if (arrObj.length > 1) {
-　　　　　　var arrPara = arrObj[1].split("&");
-　　　　　　var arr;
+// 　　　　if (arrObj.length > 1) {
+// 　　　　　　var arrPara = arrObj[1].split("&");
+// 　　　　　　var arr;
 
-　　　　　　for (var i = 0; i < arrPara.length; i++) {
-　　　　　　　　arr = arrPara[i].split("=");
+// 　　　　　　for (var i = 0; i < arrPara.length; i++) {
+// 　　　　　　　　arr = arrPara[i].split("=");
 
-　　　　　　　　if (arr != null && arr[0] == paraName) {
-　　　　　　　　　　return arr[1];
-　　　　　　　　}
-　　　　　　}
-　　　　　　return "";
-　　　　}
-　　　　else {
-　　　　　　return "";
-　　　　}
-　　}
+// 　　　　　　　　if (arr != null && arr[0] == paraName) {
+// 　　　　　　　　　　return arr[1];
+// 　　　　　　　　}
+// 　　　　　　}
+// 　　　　　　return "";
+// 　　　　}
+// 　　　　else {
+// 　　　　　　return "";
+// 　　　　}
+// }
+
+window.GetUrlParam = function(name){
+    var url = document.location.toString();
+    if(!name){
+        return '';
+    }
+    url = url || location.search;
+    name = name.replace(/(?=[\\^$*+?.():|{}])/, '\\');
+    var reg = new RegExp('(?:[?&]|^)' + name + '=([^?&#]*)', 'i');
+    var match = url.match(reg);
+    return !match ? '' : match[1];
+}

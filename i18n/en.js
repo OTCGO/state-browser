5a587b6df1cd6a67885b6fbe6b0069a5304d84d1
503945930 <503945930@qq.com>
2018-03-16 17:05:17 +0800

    add locale

--- i18n/en.js
+++ i18n/en.js
@@ -1,9 +1,81 @@
-//英文
+//简体中文
 const en = {
-  nav: {
-    explor:'Explorer',
-    assets:'Assets',
-    api:'API',
-    language:'Language',
+  menu: {
+    home: 'Home',
+    assets: 'Asset',
+    api: 'API',
+    language: 'Language',
+  },
+  slider: {
+    dynamic: {
+      name: 'Dynamic',
+      search:'Block Height, Hash, Address or Transaction id'
+    },
+    market: {
+      name: 'Market',
+    },
+    transaction: {
+      name: 'Transactions',
+    },
+    block: {
+      name: 'Blocks',
+    },
+    address: {
+      name: 'Addresses'
+    }
+  },
+  dynamic:{
+    startTime:'start time',
+    runTime:'run time',
+    blockNum:'total blocks',
+    assetNum:'total assets',
+    addressNum:'total addresses',
+    transactionNum:'total transactions',
+    day:'day'
+  },
+  block: {
+    name:'block info',
+    index:'index',
+    hash:'hash',
+    version:'version',
+    merkleroot:'merkleroot',
+    invocationScript:'invocationScript',
+    verificationScript:'verificationScript',
+    height: 'height',
+    size: 'size',
+    transactions: 'transactions',
+    time: 'time'
+  },
+  transaction: {
+    name:'transaction info',
+    type: 'type',
+    networkFee:'networkFee',
+    systemFee:'systemFee',
+    transactionId: "transaction id",
+    time: 'time',
+    size: 'size',
+    invocationScript:'invocationScript',
+    verificationScript:'verificationScript',
+    sentFrom:'sentFrom',
+    sentTo:'sentTo'
+  },
+  address: {
+    value: 'address',
+    blockIndex: 'blockIndex',
+    time: 'time'
+  },
+  asset: {
+    name: 'name',
+    type: 'type',
+    assetId: 'assetId',
+    amount: 'amount'
+  },
+  pagination:{
+    current:'current',
+    to:'to',
+    total:'total'
+  },
+  footer:{
+    version:'cli version'
   }
 }
